fb1f6c64d55909c5c7281f53bf93af794a22e855
Seantaysatay <taysean25@gmail.com>
2023-01-13 08:11:41 +0000

    final heatmap chart js file

--- heatmap_chart.js
+++ heatmap_chart.js
@@ -1,56 +1,56 @@
 const options_heat =  {
-    chart: {
-        type: 'treemap',
-        height:"70%",
-        width: '70%'
-    },
-    series:[
-        
-    ],
-    noData: {
-        "text": "Loading..."
-    },
-    plotOptions: {
-        treemap: {
-          enableShades: true,
-          shadeIntensity: 0.8,
-          reverseNegativeShade: true,
-          colorScale: {
-            ranges: [
-              {
-                from: 6000,
-                to: 12000,
-                color:'#52B12C'   //red
-              },
-              {
-                from: 0,
-                to: 6000,
-                color: '#CD363A' //green
-              }
-            ]
-          }
+  chart: {
+      type: 'treemap',
+      height:"90%",
+      width: '80%'
+  },
+  series:[
+      
+  ],
+  noData: {
+      "text": "Loading..."
+  },
+  plotOptions: {
+      treemap: {
+        enableShades: true,
+        shadeIntensity: 0.8,
+        reverseNegativeShade: true,
+        colorScale: {
+          ranges: [
+            {
+              from: 6000,
+              to: 12000,
+              color:'#52B12C'   //red
+            },
+            {
+              from: 0,
+              to: 6000,
+              color: '#CD363A' //green
+            }
+          ]
         }
       }
-   
-    
-}
+    }
  
+  
+}
+
 // create the chart
 const chart_heat = new ApexCharts(document.querySelector('#heatmap'), options_heat);
- 
+
 // render the chart
 chart_heat.render()
 
 //Load in the data
 window.addEventListener('DOMContentLoaded', async ()=>{    
-    // alert('DOMContentLoaded')
+  // alert('DOMContentLoaded')
 
-    let rawData = await loadData();
-    let series = transformData_heatmap(rawData);
-    //console.log(typeof(series))
-      
-    chart_heat.updateSeries([{
-        'name': 'Transactions',
-        'data': series
-    }])
+  let rawData = await loadData();
+  let series = transformData_heatmap(rawData);
+  //console.log(typeof(series))
+    
+  chart_heat.updateSeries([{
+      'name': 'Transactions',
+      'data': series
+  }])
 })
